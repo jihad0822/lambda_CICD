28459a4d8a535b89d0c3b6f37a7ac17ae44c968d
Jihad Muhammad <jihadmuhammad822@gmail.com>
2025-06-30 10:49:22 -0400

    intitial set up for lamda and github actions

--- lamda/lamda_function.py
+++ lamda/lamda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from vscode Lambda!')
+        'body': json.dumps('Hello from updated vscode Lambda!')
     }
